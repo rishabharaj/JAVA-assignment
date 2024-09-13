f0c26fd612655e9118dbcedd45877c998d61d4f6
Rishabharaj Sharma <137804352+rishabharaj@users.noreply.github.com>
2024-09-13 12:56:39 +0530

    Add files via upload

---
+++ java class question/multitheading2.java
@@ -0,0 +1,10 @@
+class MyRunnable implements Runnable {
+    public void run() {
+        System.out.println("Thread is running");
+    }
+    public static void main(String[] args) {
+        MyRunnable myRunnable = new MyRunnable();
+        Thread t1 = new Thread(myRunnable);
+        t1.start();
+    }
+}
